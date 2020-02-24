0595f115433c0e91f9c78fdf2fe04037d7592add
Gouri <gouri.josh@gmail.com>
2020-02-24 11:23:40 -0800

    Updates

---
+++ precache-manifest.53ee592a268203d194f661249872d41a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30ea63ad9e0704b16d52950a2d5da6ec",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "331a59affbd729afcf7c",
+    "url": "/ClickyGame/static/css/main.8b6d2bd7.chunk.css"
+  },
+  {
+    "revision": "c9b83e46fc65cf08a22a",
+    "url": "/ClickyGame/static/js/2.a9a78445.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ClickyGame/static/js/2.a9a78445.chunk.js.LICENSE"
+  },
+  {
+    "revision": "331a59affbd729afcf7c",
+    "url": "/ClickyGame/static/js/main.6d5d82c1.chunk.js"
+  },
+  {
+    "revision": "b2dcd9637bbce2c1462b",
+    "url": "/ClickyGame/static/js/runtime-main.a8c8c49b.js"
+  }
+]);
